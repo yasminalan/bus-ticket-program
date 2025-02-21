9d70c4ca00bae1924c0774f9b5ef9e70b4392471
Yasemin Alan <yaseminalan1996@gmail.com>
2025-02-21 15:11:19 +0100

    Final version-add scanner methods

--- src/TicketRunner.java
+++ src/TicketRunner.java
@@ -1,10 +1,34 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class TicketRunner {
     public static void main(String[] args) {
 
-        Ticket ticket = new Ticket(10, "Yasemin Alan", 10, 2, 3);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Distance (km): ");
+        int distance = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Passenger name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Trip type? (One-way: 1, Round trip: 2): ");
+        int tripType = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Seat number: ");
+        int seatNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        Ticket ticket = new Ticket(distance, name, age, tripType, seatNumber);
 
         double price = ticket.calculatePrice();
-        System.out.println(price);
+        System.out.println("You should pay: " + price + " EUR");
 
     }
 }
